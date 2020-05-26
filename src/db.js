90a0a09839fe91e520072e13898aff0c90a4c3ea
Christian Joshua Catalan <cj.catalan07@gmail.com>
2020-05-26 21:40:37 +0800

    Setup for docker

--- src/db.js
+++ src/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
-const config = { useNewUrlParser: true, useUnifiedTopology: true };
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  connectTimeoutMS: 10000,
+};
 
-const connect = (mongoDbUri) => mongoose.connect(mongoDbUri, config)
+const connect = (mongoDbUri) => mongoose.connect(mongoDbUri, options)
   .then(() => console.log(`Successfully connected to ${mongoDbUri}`))
   .catch(error => console.error(error));
 
